f18407ffeb02f2112365c61214b0fb5e0cc7bbc1
Sean Reed <seanreed1111@gmail.com>
2014-07-03 15:25:54 -0400

    further tinkering on httpPostRequest but no joy. Still can't parse data

--- httpRequestDemo/postRequest/postRequest/hRDViewController.m
+++ httpRequestDemo/postRequest/postRequest/hRDViewController.m
@@ -19,17 +19,17 @@
     [super viewDidLoad];
     
 	// Do any additional setup after loading the view, typically from a nib.
-    self.userId = @"Captain Caveman";
-    self.longitude = @"51.5072° N";
-    self.latitude = @"0.1275° W";
+    self.userId = @"bbbbbbbbbbbbbbbb";
+    self.longitude = [NSNumber numberWithDouble:2.1];
+    self.latitude = [NSNumber numberWithDouble:2.1];
     self.radius = [NSNumber numberWithDouble:2.1];
     self.data = [NSMutableData dataWithCapacity:0];
 
     
-    NSString *website = @"http://protected‐wildwood-8664.herokuapp.com/";
+    NSString *website = @"http://protected‐wildwood-8664.herokuapp.com/users.json";
 
     //convert encoding on website string to proper NSUTF8StringEncoding to prevent malformed NSURLMutableRequest object
-    
+
     self.url = [[NSURL alloc]initWithString:[website
                                              stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
     self.request = [NSMutableURLRequest requestWithURL:self.url];
@@ -47,31 +47,67 @@
                               @"latitude":self.latitude,
                               @"longitude":self.longitude,
                               @"radius":self.radius},
-                    @"commit":@"CreateUser",
-                    @"action":@"update",
-                    @"controller":@"users"};
-    
+                    @"commit":@"CreateUser"
+                    };
+
+    //                     @"controller":@"users"                     @"action":@"create"
     // modify request object for post request
+    
     NSData *postData = [NSJSONSerialization dataWithJSONObject:userDetails options:NSJSONWritingPrettyPrinted error:nil];
- 
+    
+    
     if(self.request && postData)     //set up connection object and connect!
     {
         [self.request setHTTPMethod:@"POST"];
         [self.request setHTTPBody:postData];
+        [self.request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        [self.request setValue:[NSString stringWithFormat:@"%d", [postData length]] forHTTPHeaderField:@"Content-Length"];
         
-        NSLog(@"Final NSURLRequest Object is %@\n",[self.request description]);
+        NSLog(@"Method is %@", [self.request HTTPMethod]);
+        NSLog(@"Final Header Fields are %@\n",[self.request allHTTPHeaderFields]);
+        NSLog(@"URL is %@", [self.request URL]);
+        
+        
+//        NSLog(@"HTTP Method is %@\n",[self.request HTTPMethod]);
+//        NSLog(@"HTTP Body is %@\n",[self.request HTTPBody]);
+
         
         self.connection = [NSURLConnection connectionWithRequest:self.request delegate:self];
     }
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 #pragma mark implement NSURLConnectionDelegate and NSURLConnectionData Delegate methods
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    // Connection succeeded.
+
+    // do something with the data HERE!!!
+    // change the label on the button
+
+    
+    NSLog(@"Connection succeeded! Received %d bytes of data\n", [self.data length]);
+    
+    // lets try to find out what is in self.data
+    
+    NSMutableString *dataString = [[NSMutableString alloc]init];
+//    NSLog(@"Response as a string is %@", [dataString initWithData:self.data encoding:NSUnicodeStringEncoding]);
+    
+    
+    
+    NSLog(@"Data is valid JSON Object? %@",[NSJSONSerialization isValidJSONObject:self.data] ? @"YES" : @"NO");
+
+    // convert response data received from website to json
+    
+    NSJSONSerialization *response = [NSJSONSerialization JSONObjectWithData:self.data options:NSJSONReadingMutableContainers error:nil];
+    
+    NSLog(@"%@\nResponse data is:\n", response);
+    
+    NSLog(@"Closing connection...");
+    
+    self.connection = nil;
+    self.data = nil;
+}
 
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
 {
@@ -104,27 +140,6 @@
     
     NSLog(@"Connection failed! Error - %@ %@",
           [error localizedDescription],[[error userInfo] objectForKey:NSURLErrorFailingURLErrorKey]);
-}
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection
-{
-    // Connection succeeded.
-    // Delegate receives no further messages for the connection
-    //Release the NSURLConnection object.
-    
-    
-    // do something with the data HERE!!!
-    // change the lable on the button
-
-    
-    NSLog(@"Connection succeeded! Received %d bytes of data\n", [self.data length]);
-    
-    NSLog(@"%@\n",[self.data description]);
-    
-    NSLog(@"Closing connection...");
-    
-    self.connection = nil;
-    self.data = nil;
 }
 
 @end
